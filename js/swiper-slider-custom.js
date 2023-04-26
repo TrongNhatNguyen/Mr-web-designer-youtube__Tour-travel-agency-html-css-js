4d682e3286e51972fac173752300b62d69eed3fa
TrongNhatNguyen <nguyentrongnhat240700@gmail.com>
2023-04-26 20:52:21 +0700

    hoàn thành phần liên hệ - contact và phần thương hiệu - brand

--- js/swiper-slider-custom.js
+++ js/swiper-slider-custom.js
@@ -1,3 +1,6 @@
+// =========================================
+//    Trang Đánh giá - REVIEW
+// =========================================
 var swiper = new Swiper('.review-slider', {
     loop: true,
     spaceBetween: 30,
@@ -24,6 +27,39 @@ var swiper = new Swiper('.review-slider', {
         },
         1024: {
             slidesPerView: 3,
+        },
+    },
+});
+
+// =========================================
+//    Trang Thương Hiệu - BRAND
+// =========================================
+var swiper = new Swiper('.brand-slider', {
+    loop: true,
+    spaceBetween: 30,
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        // ĐIỆN THOẠI DỌC MAX 180px -> 479px
+        0: {
+            slidesPerView: 2,
+        },
+        // ĐIỆN THOẠI NGANG MAX 480px -> 767px
+        580: {
+            slidesPerView: 3,
+        },
+        // ĐIỆN THOẠI NGANG MAX 768px -> 1023
+        768: {
+            slidesPerView: 4,
+        },
+        1024: {
+            slidesPerView: 4,
         },
     },
 });
